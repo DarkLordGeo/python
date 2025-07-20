481e9a43d4fdb3ab0c6cde9bb64577e420755784
DarkLordGeo <148902362+DarkLordGeo@users.noreply.github.com>
2025-07-20 15:30:01 +0400

    scraping

---
+++ scrap.py
@@ -0,0 +1,41 @@
+from bs4 import BeautifulSoup
+import requests
+import json
+
+def webInfo():
+        url = input("Enter website url: ")
+        html = requests.get(url.replace(" ",""))
+        soup = BeautifulSoup(html.text, 'html.parser')
+        
+        imgArr = soup.find_all('img')
+        imgSrc = []
+        for img in imgArr:
+            imgSrc.append(img['src'])
+        
+        hrefArr = soup.find_all('a')
+        hrefs = []
+        for href in hrefArr:
+            hrefs.append(href['href'])
+        
+        inputArr = soup.find_all('input')
+        inputs = []
+        for inputTag in inputArr:
+            inputs.append(inputTag['type'])
+        
+        webData = {
+            "data":{
+                "img_tags": [len(imgSrc),imgSrc],
+                "a_tags": [len(hrefs),hrefs],
+                "input_tags":len(inputs)
+            }
+
+        }
+        # print(webData)
+        jsonData = json.dumps(webData, indent=4)
+        
+        
+        with open('webData.json',  mode='w', encoding='utf-8') as write_file:
+            write_file.write(jsonData)
+
+
+webInfo()
